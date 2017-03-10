8332803aa7b7584a6f7501a46a70aba711887d95
DanielSzalai <cortexke@gmail.com>
2017-03-10 17:49:44 +0100

    #1

---
+++ src/hu/helix/Main.java
@@ -0,0 +1,18 @@
+package hu.helix;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // téglalap kerülete
+        int a=6;
+        int b=8;
+        int sum=2*(a+b);
+        int sum2=a*b;
+        System.out.print("a téglalap kerülete:");
+        System.out.println(sum);
+
+        //téglalap területe
+       System.out.print("a téglalap területe:");
+        System.out.print(sum2);
+    }
+}
