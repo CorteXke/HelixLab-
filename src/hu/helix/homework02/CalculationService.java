a1a168bdd83958f60767652aa7b82edff7c743db
DanielSzalai <cortexke@gmail.com>
2017-03-26 16:48:06 +0200

    #7

---
+++ src/hu/helix/homework02/CalculationService.java
@@ -0,0 +1,25 @@
+package hu.helix.homework02;
+
+
+/**
+ * Created by Dániel on 2017. 03. 25..
+ */
+public class CalculationService {
+        public boolean isItEven(int number) {
+            if (number%2==0)
+            return true;
+            else
+                return false;
+        }
+
+
+        public static void calculate(int x , int y, char operation){
+
+
+
+
+
+
+        }
+}
+
