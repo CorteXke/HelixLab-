500f1af2e0cbd2a4397d17b40a092422813212dd
DanielSzalai <cortexke@gmail.com>
2017-04-01 12:36:40 +0200

    #2

--- src/hu/helix/Main.java
+++ src/hu/helix/Main.java
@@ -1,9 +1,16 @@
 package hu.helix;
 
+import hu.helix.homework02.CalculationHelper;
+import hu.helix.homework02.CalculationService;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // téglalap kerülete
+     public static void main(String[] args) {
+
+
+
+        /*
+         // téglalap kerülete
         int a=6;
         int b=8;
         int sum=2*(a+b);
@@ -17,24 +24,24 @@ public class Main {
         System.out.println("\n");
    //második feladat, dl átváltása
         // a dl határoztam meg konstansként és azt szoroztam illetve osztottam
-
+*/
+        //2. feladat dl átváltás
         int deci=26;
-        double liter=deci*0.1;
-        double milliliter=deci*100;
-        double centiliter=deci*10;
-        double hektoliter=deci*0.001;
-         System.out.println("26 dl =" +liter + " liter");
-         System.out.println("26 dl=" +milliliter +" ml");
-         System.out.println("26dl="+hektoliter + " hl");
-         System.out.println("26dl=" +centiliter+ " cl");
-         System.out.println("\n");
+        double hektoliter = (double) Math.round((deci * 0.001) * 1000000) / 1000000;
+        double liter= (double) Math.round((deci*0.1)* 1000000) / 1000000;
+        double mililiter= (double) Math.round((deci*100)*100000/100000);
+        double centiliter= (double) Math.round((deci*10)*100000/100000);
+        System.out.println("26dl= " +hektoliter + " hl") ;
+        System.out.println("26dl= " + liter + " l");
+        System.out.println("26dl= " +mililiter + " ml") ;
+        System.out.println("26dl= " +centiliter + " cl") ;
 
-
+/*
          // 3. feladat, gömb térfogat számitás
 
         double PI=Math.PI;
         double r=12;
-        System.out.println("a"+r + "sugarú gömb térfogata= "+4*r*PI);
+        System.out.println("a"+r + "sugarú gömb térfogata= "+4 * r^3 * PI /3);
         System.out.println("\n");
 
         //4. feladat Fahrenheit Celsius váltás
@@ -61,7 +68,8 @@ public class Main {
 * frame 	*
 ************
 * */
-         System.out.println(" ********* \n * Hello * \n * World * \n * in    * \n * a     * \n * frame * \n ********* \n");
+        /*
+        System.out.println(" ********* \n * Hello * \n * World * \n * in    * \n * a     * \n * frame * \n ********* \n");
 
 
 
@@ -78,8 +86,7 @@ public class Main {
 
 
 
-
+*/
     }
 
 }
-
