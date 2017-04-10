1ba04305e3edf763d10544e340b1c035fbe4db55
DanielSzalai <cortexke@gmail.com>
2017-04-10 09:55:10 +0200

    #11

--- src/hu/helix/Main.java
+++ src/hu/helix/Main.java
@@ -1,90 +1,21 @@
 package hu.helix;
 
-import hu.helix.homework02.CalculationHelper;
-import hu.helix.homework02.CalculationService;
+import hu.helix.homework03.PasswordGenerator;
+import hu.helix.homework03.User;
+import hu.helix.homework03.UserRegistry;
 
 public class Main {
 
      public static void main(String[] args) {
 
 
-
-        /*
-         // téglalap kerülete
-        int a=6;
-        int b=8;
-        int sum=2*(a+b);
-        int sum2=a*b;
-        System.out.print("a téglalap kerülete:");
-        System.out.println(sum);
-
-        //téglalap területe
-       System.out.print("a téglalap területe:");
-        System.out.println(sum2);
-        System.out.println("\n");
-   //második feladat, dl átváltása
-        // a dl határoztam meg konstansként és azt szoroztam illetve osztottam
-
-      /*  //2. feladat dl átváltás
-        int deci=26;
-        double hektoliter = (double) Math.round((deci * 0.001) * 1000000) / 1000000;
-        double liter= (double) Math.round((deci*0.1)* 1000000) / 1000000;
-        double mililiter= (double) Math.round((deci*100)*100000/100000);
-        double centiliter= (double) Math.round((deci*10)*100000/100000);
-        System.out.println("26dl= " +hektoliter + " hl") ;
-        System.out.println("26dl= " + liter + " l");
-        System.out.println("26dl= " +mililiter + " ml") ;
-        System.out.println("26dl= " +centiliter + " cl") ;*/
+         UserRegistry userRegistry = new UserRegistry();
 
 
-         // 3. feladat, gömb térfogat számitás
-
-         double r=12;
-         System.out.println("a "+r + " sugarú gömb térfogata= "+4 * r*r*r * Math.PI /3);
-       /*
-        //4. feladat Fahrenheit Celsius váltás
-
-        int cels = (55- 32) * 5 / 9;
-        int fah= 9 * (41/ 5) + 32;
-
-        System.out.println("55 Fahrenheit = "+cels +" celsius");
-        System.out.println("41 celsius=" +fah +" fahrenheit");
-        System.out.println("\n");
+       userRegistry.run();
 
 
-        /*5. feladat Írasd ki a következő kimenetet egyetlen System.out.println(); metódus hívás segítségével.
-
-        Tipp: escape sequences
-
-        Output
-
-                ************
-* Hello 	*
-* World	*
-* in    		*
-* a     	*
-* frame 	*
-************
-* */
-        /*
-        System.out.println(" ********* \n * Hello * \n * World * \n * in    * \n * a     * \n * frame * \n ********* \n");
+     }
 
 
-
-         //6. faladat ASCII kód kiiratása
-
-        char character= 'ä';
-        int ascii= (int) character;
-        System.out.println("a ä karakter ASCII kódja: "+ascii);
-
-
-
-
-
-
-
-
-*/
-    }
-
-}
+     }
--- src/hu/helix/homework03/PasswordGenerator.java
+++ src/hu/helix/homework03/PasswordGenerator.java
@@ -2,65 +2,18 @@ package hu.helix.homework03;
 
 import java.util.Random;
 
-/**
- * Created by Dániel on 2017. 04. 01..
- */
-
-/*Sajnos a szájbarágós segitséged után is csak ennyire voltam képes */
-
 public class PasswordGenerator {
+    
+    public static String generatePassword(int length) {
 
-    public static String generatePassword() {
-
-        int length = 10;
         Random rng = new Random();
-        int [] jelszohossz=new int[65];
-        String jelszo="a";
-        String possibleCharacters = (new String("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?%"));
+        String result="";
+        String possibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?%";
 
-
-        for (int i = 0; i <= length; i++) {
-
-
-            int s = rng.nextInt(65);
-            jelszohossz[i]=s;
-            System.out.println(s);
+        for (int i = 0; i < length; i++) {
+            int s = rng.nextInt(possibleCharacters.length());
+            result += possibleCharacters.charAt(s);
         }
-
-
-
-        return jelszo;
+        return result;
     }
 }
-
-
-    /*  String jelszo=new String();*/
-     /*  int length=10;
-    Random rng=new Random();
-    String jelszo=new String();
-     char kisbetu = (char)(rng.nextInt(26) + 'a');
-     char nagybetu = (char)(rng.nextInt(26) + 'A');
-     int szamok =  (rng.nextInt(9)+0);
-     String jelek = "?%!";
-
-
-
-    jelszo= String.valueOf(kisbetu)+String.valueOf(nagybetu)+String.valueOf(szamok)+jelek;
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
